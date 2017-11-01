d1830f56379e00cf59f8b2aa6fb2ed20c1a98b7a
Juanda Uribe <j.uribe@globant.com>
2017-11-01 16:20:59 -0500

    Revert "Redux stuff added partially"
    This reverts commit e1949d5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { NgRedux, NgReduxModule } from '@angular-redux/store';
-
-import { IAppState, rootReducer, INITIAL_STATE } from './store';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-    declarations: [
-        AppComponent
-    ],
-    imports: [
-        BrowserModule,
-        NgReduxModule
-    ],
-    providers: [],
-    bootstrap: [AppComponent]
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppModule {
-    constructor(ngRedux: NgRedux<IAppState>) {
-        ngRedux.configureStore(rootReducer, INITIAL_STATE);
-    }
-}
+export class AppModule { }
